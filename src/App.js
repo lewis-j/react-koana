bc36ba6e37026d1d755210ba656ff96b141b68b6
Lindsey Jackson <lindseylewisjackson@gmail.com>
2022-07-07 21:03:16 -0700

    testing 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import './App.css';
+import "./App.css";
 
 const App = () => {
   return (
     <div>
-      <div className="test1">KOANA<div className="test2">V2</div></div>
+      <div className="test1">
+        KOANA<div className="test2">V2</div>
+      </div>
+      <div className="test1">
+        KOANA<div className="test2">V2</div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
