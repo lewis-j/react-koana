9802d08f3d8ca02842423794f57fc552152535b6
Lindsey Jackson <lindseylewisjackson@gmail.com>
2022-07-07 21:35:38 -0700

    added v3

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import "./App.css";
 const App = () => {
   return (
     <div>
-      <div className="test1">KOANA<div className="test2">V2</div></div>
+      <div className="test1">
+        KOANA<div className="test2">V2</div>
+      </div>
       <div>YeAH THIS IS A NEW PART!</div>
       <div className="test1">
         KOANA hey man<div className="test2">V2 is so sweeet</div>
@@ -11,6 +13,7 @@ const App = () => {
       </div>
       <div className="test1">
         KOANA<div className="test2">V2</div>
+        <div className="test2">V3</div>
       </div>
     </div>
   );
