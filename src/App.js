1b97f0237e97e19e57d9abe86f51d48e4ca5ae31
thejourneyville <bmalberg@protonmail.com>
2022-07-07 21:35:06 -0700

    I love v8

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
         KOANA<div className="test2">V2</div>
       </div>
       <div className="test1">
-        KOANA<div className="test2">V2</div>
+        KOANA<div className="test2">V8 is my favorite tomato beverage</div>
       </div>
     </div>
   );
