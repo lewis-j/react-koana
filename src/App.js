2e72af760e1359a148aaa973c732ae1322a23d5c
thejourneyville <bmalberg@protonmail.com>
2022-07-13 12:04:42 -0700

    updated app.js

--- src/App.js
+++ src/App.js
@@ -12,12 +12,9 @@ const App = () => {
         KOANA<div className="test2">V2</div>
       </div>
       <div className="test1">
-<<<<<<< HEAD
         KOANA<div className="test2">V2</div>
         <div className="test2">V3</div>
-=======
         KOANA<div className="test2">V8 is my favorite tomato beverage</div>
->>>>>>> 1b97f0237e97e19e57d9abe86f51d48e4ca5ae31
       </div>
     </div>
   );
