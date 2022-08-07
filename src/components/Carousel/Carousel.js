9c38d840cd688678593b0a14dffc43e1b15cf034
thejourneyville <bmalberg@protonmail.com>
2022-08-07 13:31:57 -0700

    newest update copied from slack

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -3,35 +3,23 @@ import React, { useState } from "react";
 import { useTransitionStyle } from "./CarouselHooks";
 import styles from "./Carousel.module.scss";
 const _style = (...styles) => styles.join(" ");
-
 const Carousel = ({
-    children: items = ["1", "2"],
-    className: wrapper = {},
-    slidesToShow = 1,
-    slidesToMove = 1,
-    prevBtnStyles,
-    nextBtnStyles,
+  children: items = ["1", "2"],
+  className: wrapper = {},
+  slidesToShow = 1,
+  slidesToMove = 1,
+  prevBtnStyles,
+  nextBtnStyles,
 }) => {
-    const totalSlides = slidesToShow + slidesToMove;
-    console.log(items[0]);
-    const initialSlideContent = items.slice(0, totalSlides);
-    const [slides, setSlides] = useState(initialSlideContent);
-    const [currentX, setCurrentX] = useState(0);
-    const [currentY, setCurrentY] = useState(0);
-    const [stage, setStage] = useState("IDLE");
-    const sliderWidth = `${((totalSlides / slidesToShow) * 100).toFixed(0)}%`;
-    const transformX = `${((slidesToMove / totalSlides) * 100).toFixed(0)}%`;
-
+  const totalSlides = slidesToShow + slidesToMove;
   const initialSlideContent = items[0].slice(0, totalSlides);
   console.log("initialSlideContent:", initialSlideContent);
-
   const [slides, setSlides] = useState(initialSlideContent);
   const [current, setCurrent] = useState({ x: 0, y: 0 });
   const [rowPosition, setRowPosition] = useState(
     [...Array(items.length).keys()].map(() => 0)
   );
   const [transition, setTransition] = useState({});
-
   const setNextCurrent = (current, action) => {
     return {
       next: { ...current, x: current.x + slidesToMove },
@@ -46,19 +34,15 @@ const Carousel = ({
       },
     };
   };
-
   const runTransition = ([stageTransition, transition]) => {
     setTransition(stageTransition);
     setTimeout(() => {
       setTransition(transition);
     }, 1);
   };
-
   const sliderWidth = `${((totalSlides / slidesToShow) * 100).toFixed(0)}%`;
   const transformX = `${((slidesToMove / totalSlides) * 100).toFixed(0)}%`;
-
   const transitions = useTransitionStyle(transformX);
-
   const slideNext = () => {
     if (current.x < items[current.y].length - slidesToShow) {
       setSlides(() =>
@@ -90,10 +74,8 @@ const Carousel = ({
     );
     setRowPosition((rp) => {
       console.log({ rp: rp, "current.x": current.x });
-
       rp[current.y] = current.x;
       console.log("rp:", rp);
-
       return rp;
     });
     setCurrent((current) => ({
@@ -122,7 +104,6 @@ const Carousel = ({
     }));
     runTransition(transitions.up);
   };
-
   return (
     <div className={wrapper}>
       <div className={styles.container}>
@@ -137,23 +118,24 @@ const Carousel = ({
             <div key={`#slide${i}`} className={styles.slides}>
               {slide}
             </div>
-
-            <button
-                onClick={() => slidePrev()}
-                className={_style(styles.btn, styles.btnPrev)}
-            >
-                Prev
-            </button>
-            <button
-                onClick={() => slideNext()}
-                className={_style(styles.btn, styles.btnNext)}
-            >
-                Next
-            </button>
-            <button onClick={() => slideDown()}>Down</button>
-            <button onClick={() => slideUp()}>Up</button>
+          ))}
         </div>
-    );
+      </div>
+      <button
+        onClick={() => slidePrev()}
+        className={_style(styles.btn, styles.btnPrev)}
+      >
+        Prev
+      </button>
+      <button
+        onClick={() => slideNext()}
+        className={_style(styles.btn, styles.btnNext)}
+      >
+        Next
+      </button>
+      <button onClick={() => slideDown()}>Down</button>
+      <button onClick={() => slideUp()}>Up</button>
+    </div>
+  );
 };
-
 export default Carousel;
