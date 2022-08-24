df0b391e7ef84df65d8a9601c5c4a2803bcb12d7
thejourneyville <bmalberg@protonmail.com>
2022-08-24 00:04:41 -0700

    refactoring of icon and badge components for more general use

--- src/Components/Cart/CartBadge.js
+++ src/Components/Cart/CartBadge.js
@@ -1,20 +1,15 @@
-import { useContext } from "react";
-import { CartContext } from "../../context/CartContext";
 import "./cartBadge.css";
 
-const CartBadge = () => {
-    const { getCartData, handleDisplayCart } = useContext(CartContext);
-    const cartItemQuantity = getCartData().reduce((acc, cur) => {
-        return cur.quantity + acc;
-    }, 0);
+const CartBadge = ({ quantity, action }) => {
+    // const { getCartData, handleDisplayCart } = useContext(CartContext);
+    // const cartItemQuantity = getCartData().reduce((acc, cur) => {
+    //     return cur.quantity + acc;
+    // }, 0);
 
     return (
-        cartItemQuantity > 0 && (
-            <div
-                className="cartBadgeContainer"
-                onClick={() => handleDisplayCart()}
-            >
-                {cartItemQuantity}
+        quantity > 0 && (
+            <div className="cartBadgeContainer" onClick={() => action()}>
+                {quantity}
             </div>
         )
     );
--- src/layout/NavMenu/GetIcon.js
+++ src/layout/NavMenu/GetIcon.js
@@ -1,17 +1,15 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import CartBadge from "../../components/Cart/CartBadge";
 import { CartContext } from "../../context/CartContext";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faCartArrowDown } from "@fortawesome/free-solid-svg-icons";
 import { useContext } from "react";
 
-export const GetIcon = ({ faInstagram, faCartArrowDown }) => {
-    const value = useContext(CartContext);
+export const GetIcon = () => {
+    const { getCartData, handleDisplayCart } = useContext(CartContext);
 
     const iconsData = [
-        [faCartArrowDown, () => value.handleDisplayCart()],
-        // (below) is there an issue with this 'window.open' call (as opposed to
-        // window.location.href) ? I used it to allow for the
-        // "_blank" suffix so clicking on the instagram icon would open a new tab
-        // https://stackoverflow.com/questions/26605170/whats-the-difference-between-window-openurl-and-window-location-href-url-on
+        [faCartArrowDown, () => handleDisplayCart()],
         [
             faInstagram,
             () =>
@@ -19,18 +17,30 @@ export const GetIcon = ({ faInstagram, faCartArrowDown }) => {
         ],
     ];
 
-    return iconsData.map(([icon, destination], iconIdx) => {
+    const iconWithBadge = ["cart-arrow-down"];
+    const cartItemQuantity = getCartData().reduce((acc, cur) => {
+        return cur.quantity + acc;
+    }, 0);
+
+    return iconsData.map(([icon, action], iconIdx) => {
+        console.log(icon.iconName);
         return (
             <div key={iconIdx}>
-                {icon.iconName.includes("cart") &&
-                    value.getCartData().length > 0 && <CartBadge />}
+                {iconWithBadge.includes(icon.iconName) && (
+                    <CartBadge quantity={cartItemQuantity} action={action} />
+                )}
                 <FontAwesomeIcon
                     icon={icon}
                     size="1x"
-                    onClick={destination}
+                    onClick={action}
                     style={{ cursor: "pointer" }}
                 />
             </div>
         );
     });
 };
+
+// (faInstagram) is there an issue with this 'window.open' call (as opposed to
+// window.location.href) ? I used it to allow for the
+// "_blank" suffix so clicking on the instagram icon would open a new tab
+// https://stackoverflow.com/questions/26605170/whats-the-difference-between-window-openurl-and-window-location-href-url-on
